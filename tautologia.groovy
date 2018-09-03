d24361e92babbc005b7ac204188b773dacd33c47
J. Famous <joshua.famous@gmail.com>
2018-09-02 23:42:12 -0400

    Split input into list

--- tautologia.groovy
+++ tautologia.groovy
@@ -4,5 +4,10 @@ def input = inputPane 'Enter your text :'
 assert input.length() > 0 : "Empty input"
 assert input.length() < 1000000 : "Input too long"
 
-//Organize Input
-println "Hello $input."
+//Repeat Input
+println "Got input : $input"
+
+//Split into List
+def content = input.split(" ")
+
+println "$content"
